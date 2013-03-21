8d17b814f271a82347c1fba0ee2b88337b0f4b50
skuarch <skuarch@yahoo.com.mx>
2013-03-21 00:13:57 -0600

    firts commit

---
+++ src/rabbitmqproducer/RabbitMQProducer.java
@@ -0,0 +1,33 @@
+package rabbitmqproducer;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.*;
+import java.io.IOException;
+
+/**
+ *
+ * @author skuarch
+ */
+public class RabbitMQProducer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        factory.setVirtualHost("/");
+        factory.setHost("127.0.0.1");
+        factory.setPort(5672);
+        Connection conn = factory.newConnection();
+        Channel channel = conn.createChannel();
+        String exchangeName = "myExchange";
+        String routingKey = "testRoute";
+        byte[] messageBodyBytes = "Hello, world!".getBytes();
+        channel.basicPublish(exchangeName, routingKey, MessageProperties.PERSISTENT_TEXT_PLAIN, messageBodyBytes);
+        channel.close();
+        conn.close();
+    }
+}
